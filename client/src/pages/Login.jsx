ee64b23b1edd53d2dd8178b304947057212774c2
abdulrafay-1 <abdulrock413@gmail.com>
2025-02-13 23:35:52 +0500

    fix login image

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -55,14 +55,10 @@ const Login = () => {
             <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
                 <ToastContainer />
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img
-                        alt="Your Company"
-                        src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-                        className="mx-auto h-10 w-auto"
-                    />
                     <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
                         Sign in to your account
                     </h2>
+                    <p className='text-gray-500 text-sm md:text-base text-center'>welcome to blogging app  📝</p>
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
